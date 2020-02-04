32cd0f805f39eb6fc5e3ce55d44be13cdaa35fe0
darkwolfxj <hesitation.dotdot@gmail.com>
2020-02-04 11:19:49 -0500

    idk what the changes were

--- bw_guesswho/src/components/home-page/game/Game.js
+++ bw_guesswho/src/components/home-page/game/Game.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import PresCandidates from './PresCandidates';
 import Tweet from './Tweet';
-
+import Menu from '../../Nav/Menu';
 const Game = () => {
     return (
         <div>
+            <Menu />
             <h1>Game component!!!</h1>
             <Tweet/>
             <PresCandidates/>
